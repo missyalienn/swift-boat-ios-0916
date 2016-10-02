136e78ad12fb31f960177106e3bd1401beb5150a
Missy Allan <missyallan@Melissas-Air.fios-router.home>
2016-10-02 18:53:58 -0400

    im on a boat andddd its going fast anddd

---
+++ swift-boat/Boat.swift
@@ -0,0 +1,35 @@
+//
+//  Boat.swift
+//  swift-boat
+//
+//  Created by Missy Allan on 10/2/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat {
+    
+    let name: String
+    var sailors: [String]
+    var maxSpeedKnots : Double
+    var speedKnots: Double = 0.0
+    
+    init(name: String, sailors: [String], maxSpeedKnots: Double) {
+        
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+        
+    }
+    
+    
+    convenience init(name: String, maxSpeedKnots: Double) {
+        self.init(name: name, sailors: [], maxSpeedKnots: maxSpeedKnots)
+    }
+    
+    
+    
+}
+
+
